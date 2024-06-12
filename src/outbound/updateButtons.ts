604f182a17de3b4f532e2144479548d2a398e8b4
Nick Gourlay <nick@gourlay.me>
2024-06-12 16:30:50 -0400

    fixing button inputs

--- src/outbound/updateButtons.ts
+++ src/outbound/updateButtons.ts
@@ -1,38 +1,53 @@
 import { MessageEditOptions, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } from 'discord.js';
 //
 export async function updateEmbed(originalMessage: any, mediaTitle: any, interaction: any, action: any) {
-    // Capitalize the action
-    const capitalizedAction = action.charAt(0).toUpperCase() + action.slice(1).toUpperCase();
-    // Create the updated embed
-    const updatedEmbed = {
-        title: `Media Request ${capitalizedAction}D`,
-        description: `Request for ${mediaTitle} has been ${action}d by ${interaction.user.tag}.`,
-    };
-    // Create the edit options
-    const editOptions: MessageEditOptions = {
-        embeds: [updatedEmbed],
-        components: [],
-    };
-    const requestExists = new ButtonBuilder()
-        .setCustomId('requestExists')
-        .setDisabled(true)
-        .setLabel('Request Submitted')
-        .setStyle(ButtonStyle.Danger);
+    switch (action) {
+        case 'decline': {
+            const updatedEmbed = {
+                title: `Media Request Approved`,
+                description: `Request for ${mediaTitle} has been approved by ${interaction.user.tag}.`,
+            };
+            await interaction.update({
+                embeds: [updatedEmbed],
+            });
+            break;
+        }
+        case 'approve': {
+            const updatedEmbed = {
+                title: `Media Request Approved`,
+                description: `Request for ${mediaTitle} has been approved by ${interaction.user.tag}.`,
+            };
+            await interaction.update({
+                embeds: [updatedEmbed],
+            });
+            break;
+        }
+        case 'resolved': {
+            const updatedEmbed = {
+                title: `Issue Closed`,
+                description: `Issue for ${mediaTitle} has been closed by ${interaction.user.tag}.`,
+            };
+            await interaction.update({
+                embeds: [updatedEmbed],
+            });
+            break;
+        }
+        case 'requestMedia': {
+            const requestSubmitted = new ButtonBuilder()
+                .setCustomId('requestSubmitted')
+                .setDisabled(true)
+                .setLabel('Request Submitted')
+                .setStyle(ButtonStyle.Danger);
 
-    const row = new ActionRowBuilder<ButtonBuilder>()
-        .addComponents(requestExists);
-    console.log(interaction.message.components);
-    await interaction.update({
-        components: [interaction.message.components[1], row],
-    });
-    console.log(interaction.message.components);
-
-    // // Edit the original message
-    // if (originalMessage) {
-    //     const exampleEmbed = new EmbedBuilder()
-    //         .setTitle('Some title')
-    //         .setDescription('Description after the edit');
-
-    //     originalMessage.edit({ embeds: [exampleEmbed] });
-    // }
+            const row = new ActionRowBuilder<ButtonBuilder>()
+                .addComponents(requestSubmitted);
+            await interaction.update({
+                components: [interaction.message.components[1], row],
+            });
+            break;
+        }
+        default: {
+            console.error('Invalid action provided.');
+        }
+    }
 }
